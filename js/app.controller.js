5ca9398b0b48206fd2e3c2ad9217f5acbd5a594e
IdanGez <idanngez@gmail.com>
2021-12-22 20:09:34 +0200

    final commit of the day

--- js/app.controller.js
+++ js/app.controller.js
@@ -26,6 +26,7 @@ window.onCloseModal = onCloseModal;
 window.onAddLocation = onAddLocation;
 window.onRemoveLocation = onRemoveLocation;
 window.goToMyLocation = goToMyLocation;
+window.onSearch = onSearch;
 
 function onInit() {
 	mapService
@@ -123,39 +124,6 @@ function goToMyLocation() {
 	});
 }
 
-// function getPosition() {
-//     if (!navigator.geolocation) {
-//         alert("HTML5 Geolocation is not supported in your browser.");
-//         return;
-//     }
-//     navigator.geolocation.getCurrentPosition(showLocation, handleLocationError);
-// }
-// function showLocation(position) {
-//     console.log(position);
-//     document.getElementById("latitude").innerHTML = position.coords.latitude;
-//     document.getElementById("longitude").innerHTML = position.coords.longitude;
-//     document.getElementById("accuracy").innerHTML = position.coords.accuracy;
-
-//     var date = new Date(position.timestamp);
-//     document.getElementById("timestamp").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-//     initMap(position.coords.latitude, position.coords.longitude);
-// }
-
-// function handleLocationError(error) {
-//     var locationError = document.getElementById("locationError");
-
-//     switch (error.code) {
-//         case 0:
-//             locationError.innerHTML = "There was an error while retrieving your location: " + error.message;
-//             break;
-//         case 1:
-//             locationError.innerHTML = "The user didn't allow this page to retrieve a location.";
-//             break;
-//         case 2:
-//             locationError.innerHTML = "The browser was unable to determine your location: " + error.message;
-//             break;
-//         case 3:
-//             locationError.innerHTML = "The browser timed out before retrieving the location.";
-//             break;
-//     }
-// }
+function onSearch(value){
+	mapService.goToSearchLocation(value)
+}
